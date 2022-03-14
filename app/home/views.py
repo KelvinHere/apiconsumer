c6f4a45578613b44727f57f87288656204503e9b
KelvinHere <kel@DESKTOP-N5MBM51.localdomain>
2022-03-14 11:12:41 +0000

    FEAT: Use requests to consume Random Word API

--- app/home/views.py
+++ app/home/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+import requests
 
 def home(request):
+    response = requests.get('http://46.101.13.65:8000/word')
+    api_result = response.json()
     template = "home/index.html"
-    return render(request, template)
+    context = {
+        'api_result': api_result
+    }
+    return render(request, template, context)
