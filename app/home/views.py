e6bec29477e339d536b19557fcbb7b225fdc275e
KelvinHere <kel@DESKTOP-N5MBM51.localdomain>
2022-03-29 13:37:11 +0100

    FEAT: Add error handling if no word received from API

--- app/home/views.py
+++ app/home/views.py
@@ -1,9 +1,22 @@
+from json import JSONDecodeError
+import json
 from django.shortcuts import render
 import requests
 
 def home(request):
-    response = requests.get('http://46.101.13.65:8000/word')
-    api_result = response.json()
+    try:
+        response = requests.get('http://46.101.13.65:8000/word')
+        api_result = response.json()
+    except ValueError:
+        api_result = {
+                                "id": '1',
+                                "word": "SERVER ERROR",
+                                "meaning": "PLEASE TRY AGAIN LATER"
+                              }
+
+    print(type(api_result))
+    print(api_result)
+
     template = "home/index.html"
     context = {
         'api_result': api_result
